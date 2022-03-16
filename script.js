84ae8f8ae04e2e87d5a9d27fba294d0e61d2dee3
Khalid Ali <khalludi123@gmail.com>
2022-03-16 00:02:30 -0500

    Added white up arrow on menu open

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ function myFunction() {
   var x = document.getElementById("navDropdown");
   var logo = document.getElementsByClassName("logo")[0];
   var navMenu = document.getElementsByClassName('nav-menu')[0];
+  var downArrow = document.getElementById("down-arrow");
   if (!showingDropdown) {
     x.className += " nav-show";
     logo.className += " logo-white";
     navMenu.className += " nav-menu-open";
+    downArrow.className = "up-arrow";
     showingDropdown = true;
   } else {
     x.className = "nav-dropdown";
     logo.className = "logo";
-    navMenu.className += "nav-link nav-menu";
+    navMenu.className = "nav-link nav-menu";
+    downArrow.className = "down-arrow";
     showingDropdown = false;
   }
 }
